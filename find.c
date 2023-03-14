b867bae63b7b75d6110cd6965c87258da88d4e78
Minseo <cloud47@thinkware.co.kr>
2023-03-14 15:37:23 +0900

    기본틀 만듦

--- find.c
+++ find.c
@@ -1,11 +1,31 @@
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<dirent.h>
+#include<unistd.h>
+ 
+int main(int argc, char* argv[])
+{
+		
+        char * cwd = (char *)malloc(sizeof(char) * 1024);
+        
+        DIR * dir = NULL;
+        struct dirent * entry = NULL;
+ 
+        while((entry = readdir(dir)) != NULL)
+        {
+                printf("%s\n", entry->d_name);
+        }
+ 
+        printf("\n");
+        
+        free(cwd);
+ 
+        closedir(dir);
+ 
+        return 0;
+ 
+}
 
-
-int main() {
-    (int)malloc()
-
+void PrintDirData(struct dirent* entry) {
+    printf("%s\t", entry->d_ino);
 }
