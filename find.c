de729cd9be0892d429250cc5f3561c2b39d43390
Minseo <cloud47@thinkware.co.kr>
2023-03-17 15:04:46 +0900

    파일 경로 입력시 이름 출력 구현

--- find.c
+++ find.c
@@ -1,31 +1,41 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<dirent.h>
-#include<unistd.h>
- 
-int main(int argc, char* argv[])
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <unistd.h>
+
+void PrintDirData(struct dirent *entry);
+
+int main(int argc, char *argv[])
 {
-		
-        char * cwd = (char *)malloc(sizeof(char) * 1024);
-        
-        DIR * dir = NULL;
-        struct dirent * entry = NULL;
- 
-        while((entry = readdir(dir)) != NULL)
-        {
-                printf("%s\n", entry->d_name);
-        }
- 
-        printf("\n");
-        
-        free(cwd);
- 
-        closedir(dir);
- 
-        return 0;
- 
+
+    char *cwd = (char *)malloc(sizeof(char) * 1024);
+
+    if (argc == 1) {
+        getcwd(cwd, 1024);
+    } else {
+        strcpy(cwd, argv[1]);
+    }
+
+    DIR *dir = NULL;
+    struct dirent *entry = NULL;
+
+    dir = opendir(cwd);
+
+    while ((entry = readdir(dir)) != NULL)
+    {
+        PrintDirData(entry);
+    }
+
+    printf("\n");
+
+    free(cwd);
+
+    closedir(dir);
+
+    return 0;
 }
 
-void PrintDirData(struct dirent* entry) {
-    printf("%s\t", entry->d_ino);
+void PrintDirData(struct dirent *entry)
+{
+    printf("%s\n", entry->d_name);
 }
