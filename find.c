cc9b3e1f43f513d7982d9687e9cca614bf609295
Minseo <cloud47@thinkware.co.kr>
2023-04-12 16:12:33 +0900

    시간 출력 추가

--- find.c
+++ find.c
@@ -1,41 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <unistd.h>
+#include <time.h>
+#include <dirent.h>
 
-void PrintDirData(struct dirent *entry);
+enum{
+    MAX_DIRECTORY_LENGTH = 1024
+};
+
+void PrintDirData(char* file_name, struct stat *stat_data);
 
 int main(int argc, char *argv[])
 {
+    char *cwd = (char *)malloc(sizeof(char) * MAX_DIRECTORY_LENGTH);
+    char *file_name = (char *)malloc(sizeof(char) * MAX_DIRECTORY_LENGTH);;
+    getcwd(cwd, MAX_DIRECTORY_LENGTH);
 
-    char *cwd = (char *)malloc(sizeof(char) * 1024);
+    char backslash[] = "\\";
 
-    if (argc == 1) {
-        getcwd(cwd, 1024);
-    } else {
-        strcpy(cwd, argv[1]);
+    struct stat file_status;
+    DIR *file_dir = NULL;
+    struct dirent *file = NULL;
+
+    file_dir = opendir(cwd);
+
+    if (argc != 1) {
+        strcpy(file_name, argv[1]);
+        strcat(cwd, backslash);
+        strcat(cwd, file_name);
+        stat(cwd, &file_status);
+        PrintDirData(file_name, &file_status);
     }
 
-    DIR *dir = NULL;
-    struct dirent *entry = NULL;
-
-    dir = opendir(cwd);
-
-    while ((entry = readdir(dir)) != NULL)
+    while ((file = readdir(file_dir)) != NULL)
     {
-        PrintDirData(entry);
+        stat(cwd, &file_status);
+        PrintDirData(file->d_name, &file_status);
     }
 
     printf("\n");
 
     free(cwd);
 
-    closedir(dir);
-
+    closedir(file_dir);
+    system("PAUSE");
     return 0;
 }
 
-void PrintDirData(struct dirent *entry)
+void PrintDirData(char* file_name, struct stat *stat_data)
 {
-    printf("%s\n", entry->d_name);
+    struct tm* t = localtime(&stat_data->st_ctime);
+
+    printf("%d-%d-%d\t", t->tm_year, t->tm_mon, t->tm_mday);
+    if(t->tm_hour >= 12)
+        printf("오후 %d:%d\t", t->tm_hour, t->tm_min);
+    else
+        printf("오전 %d:%d\t", t->tm_hour, t->tm_min);
+    printf("%s\n", file_name);
 }
