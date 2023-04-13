524e5372219f6b441af58ef586be34e4fe749540
Minseo <cloud47@thinkware.co.kr>
2023-04-13 12:35:46 +0900

    시리얼넘버, 드라이브레터, 날짜, 시간 구현

--- find.c
+++ find.c
@@ -5,12 +5,22 @@
 #include <unistd.h>
 #include <time.h>
 #include <dirent.h>
+#include <windows.h>
 
 enum{
-    MAX_DIRECTORY_LENGTH = 1024
+    MAX_DIRECTORY_LENGTH = 1024,
+    MAX_SERIAL_NUM_LENGTH = 9
 };
 
+typedef struct VolumeInfo {
+    char volume_name[MAX_PATH + 1];
+    char volume_serial[MAX_SERIAL_NUM_LENGTH];
+} VolInfo;
+
+void GetVolumeInfo(VolInfo* volume_info, char* cwd);
 void PrintDirData(char* file_name, struct stat *stat_data);
+
+
 
 int main(int argc, char *argv[])
 {
@@ -24,6 +34,12 @@ int main(int argc, char *argv[])
     DIR *file_dir = NULL;
     struct dirent *file = NULL;
 
+    VolInfo volume_info;
+
+    GetVolumeInfo(&volume_info, cwd);
+    
+    printf("%s\n%s\n", volume_info.volume_name, volume_info.volume_serial);
+
     file_dir = opendir(cwd);
 
     if (argc != 1) {
@@ -32,12 +48,16 @@ int main(int argc, char *argv[])
         strcat(cwd, file_name);
         stat(cwd, &file_status);
         PrintDirData(file_name, &file_status);
+        return 0;
     }
 
     while ((file = readdir(file_dir)) != NULL)
     {
+        strcat(cwd, backslash);
+        strcat(cwd, file->d_name);
         stat(cwd, &file_status);
         PrintDirData(file->d_name, &file_status);
+        getcwd(cwd, MAX_DIRECTORY_LENGTH);
     }
 
     printf("\n");
@@ -51,12 +71,33 @@ int main(int argc, char *argv[])
 
 void PrintDirData(char* file_name, struct stat *stat_data)
 {
-    struct tm* t = localtime(&stat_data->st_ctime);
+    struct tm* t = localtime(&stat_data->st_atime);
 
-    printf("%d-%d-%d\t", t->tm_year, t->tm_mon, t->tm_mday);
+    printf("%d-%d-%d\t", t->tm_year+1900, t->tm_mon+1, t->tm_mday);
     if(t->tm_hour >= 12)
-        printf("오후 %d:%d\t", t->tm_hour, t->tm_min);
+        printf("오후 %d:%d\t", t->tm_hour-12, t->tm_min);
     else
         printf("오전 %d:%d\t", t->tm_hour, t->tm_min);
     printf("%s\n", file_name);
+}
+
+void GetVolumeInfo(VolInfo* volume_info, char* cwd) {
+    
+    char rootPath[] = {cwd[0], cwd[1], cwd[2]}; 
+    DWORD vol_serial; 
+    DWORD max_comp_len; 
+    DWORD fs_flags; 
+    char fs_name[MAX_PATH + 1]; 
+
+    BOOL result = GetVolumeInformationA(
+        rootPath,
+        volume_info->volume_name, 
+        sizeof(volume_info->volume_name),
+        &vol_serial, 
+        &max_comp_len,
+        &fs_flags, 
+        fs_name,
+        sizeof(fs_name));
+
+    sprintf(volume_info->volume_serial, "%08X", vol_serial);
 }
