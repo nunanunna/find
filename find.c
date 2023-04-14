b36f1e068f3c5ca576ae4cfc09844699f85dde69
Minseo <cloud47@thinkware.co.kr>
2023-04-14 11:32:19 +0900

    dir과 출력 포맷 일치 완료

--- find.c
+++ find.c
@@ -6,6 +6,7 @@
 #include <time.h>
 #include <dirent.h>
 #include <windows.h>
+#include <locale.h>
 
 enum{
     MAX_DIRECTORY_LENGTH = 1024,
@@ -20,11 +21,13 @@ typedef struct VolumeInfo {
 void GetVolumeInfo(VolInfo* volume_info, char* cwd);
 void PrintDirData(char* file_name, struct stat *stat_data);
 void PrintVolumeInfo(VolInfo* volume_info, char* cwd);
-
+unsigned long long int GetVolumeSize(char* cwd);
 
 
 int main(int argc, char *argv[])
 {
+    setlocale(LC_NUMERIC, "");
+
     char *cwd = (char *)malloc(sizeof(char) * MAX_DIRECTORY_LENGTH);
     char *file_name = (char *)malloc(sizeof(char) * MAX_DIRECTORY_LENGTH);;
     getcwd(cwd, MAX_DIRECTORY_LENGTH);
@@ -36,6 +39,10 @@ int main(int argc, char *argv[])
     struct dirent *file = NULL;
 
     VolInfo volume_info;
+
+    int dir_count = 0;
+    int file_count = 0;
+    long total_file_size = 0;
 
     GetVolumeInfo(&volume_info, cwd);
     PrintVolumeInfo(&volume_info, cwd);
@@ -50,7 +57,7 @@ int main(int argc, char *argv[])
         PrintDirData(file_name, &file_status);
         return 0;
     }
-
+    
     while ((file = readdir(file_dir)) != NULL)
     {
         strcat(cwd, backslash);
@@ -58,14 +65,22 @@ int main(int argc, char *argv[])
         stat(cwd, &file_status);
         PrintDirData(file->d_name, &file_status);
         getcwd(cwd, MAX_DIRECTORY_LENGTH);
+
+        if(S_ISDIR(file_status.st_mode))
+            dir_count++;
+        if(S_ISREG(file_status.st_mode))
+            file_count++;
+
+        total_file_size += file_status.st_size;
     }
 
-    printf("\n");
+    printf("%'16d개 파일%'20d 바이트\n", file_count, total_file_size);
+    printf("%'16d개 디렉터리%'17llu 바이트 남음\n", dir_count, GetVolumeSize(cwd));
 
     free(cwd);
+    free(file_name);
 
     closedir(file_dir);
-    system("PAUSE");
     return 0;
 }
 
@@ -75,19 +90,19 @@ void PrintDirData(char* file_name, struct stat *stat_data)
     struct tm* t = localtime(&stat_data->st_atime);
     
 
-    printf("%d-%02d-%02d\t", t->tm_year+1900, t->tm_mon+1, t->tm_mday);
+    printf("%d-%02d-%02d  ", t->tm_year+1900, t->tm_mon+1, t->tm_mday);
     if(t->tm_hour >= 12)
-        printf("오후 %02d:%02d\t", t->tm_hour-12, t->tm_min);
+        printf("오후 %02d:%02d    ", t->tm_hour-12, t->tm_min);
     else if(t->tm_hour == 12)
-        printf("오후 %02d:%02d\t", 12, t->tm_min);
+        printf("오후 %02d:%02d    ", 12, t->tm_min);
     else
-        printf("오전 %02d:%02d\t", t->tm_hour, t->tm_min);
+        printf("오전 %02d:%02d    ", t->tm_hour, t->tm_min);
     
     // 디렉터리 여부 출력
     if(S_ISDIR(stat_data->st_mode))
-        printf("<DIR>\t\t");
+        printf("<DIR>          ");
     else
-        printf("\t%,d\t", stat_data->st_size);
+        printf("%'14d ", stat_data->st_size);
 
     printf("%s\n", file_name);
 }
@@ -124,4 +139,11 @@ void PrintVolumeInfo(VolInfo* volume_info, char* cwd) {
     volume_info->volume_serial[4], volume_info->volume_serial[5], volume_info->volume_serial[6], volume_info->volume_serial[7]);
 
     printf(" %s 디렉터리\n\n", cwd);
+}
+
+unsigned long long int GetVolumeSize(char* cwd) {
+    ULARGE_INTEGER total, free;
+    GetDiskFreeSpaceEx("C:\\", NULL, &total, &free);// C드라이브의 정보를 얻음
+
+    return free.QuadPart;
 }
